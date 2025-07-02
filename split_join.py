319a7e78701911b6e063aa59b07cac8b1d2351ac
achal2106 <2024bsc120@sggs.ac.in>
2025-07-02 10:47:38 +0530

    add file

---
+++ split_join.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+def split_and_join(line):
+    split=line.split(" ")
+    join="-".join(split)
+    return join
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
